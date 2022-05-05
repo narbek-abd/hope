6011fc410fa25eabbb92a26cb91df1b1f6742f9a
narbek <narbek.abdullayev@gmail.ru>
2022-05-05 14:05:34 +0600

    vue installed

---
+++ src/layouts/TheHeader.vue
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+
+<template>
+  <div>
+  sss
+  </div>
+</template>
+
+<style scoped>
+ 
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TheWelcome from '@/components/TheWelcome.vue'
+import TheHeader from '@/layouts/TheHeader.vue'
+</script>
+
+<template>
+  <main>
+    <TheHeader />
+  </main>
+</template>
